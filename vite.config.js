1f5b441008600dc76000bfb752fadeaf0750a8fe
zhang_liancai <zhang_liancai@leapmotor.com>
2021-02-22 17:53:42 +0800

    first commit

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+const path = require("path");
+
+module.exports = {
+	alias: {
+		// 路径映射必须以/开头和结尾
+		"/comps/": path.resolve(__dirname, "src/components"),
+	},
+	proxy: {
+		'/api': {
+			target: 'localhost:3000',
+			changeOrigin: true,
+			rewrite: path => path.replace(/^\/api/, '')
+		}
+	},
+	plugins: [
+		createMockServer({
+			supportTs: false,
+		}),
+	]
+}
